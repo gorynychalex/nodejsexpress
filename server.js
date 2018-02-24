a205bf9d8e66637b2984e7b7572fb769923ab3c4
gorynychalex <gorynychalex@gmail.com>
2018-02-24 15:36:31 +1000

    modified get post put delete methods for db.collection

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 //4.1 mongodb include and MongoClient initialize
 var MongoClient = require('mongodb').MongoClient;
 
+//4.6.1 include ObjectID
+var ObjectID = require('mongodb').ObjectID;
+
 //1.2 instance of express
 var app = express();
 
@@ -38,40 +41,61 @@ app.get('/', function (req, res) {
 
 //2.2 add base route for questions
 app.get('/questions', (req, res)=> {
-  res.send(questions);
+  
+  //4.5 insert find question
+  db.collection('questions').find().toArray((err,docs) => {
+    if(err) { console.log(err); return res.sendStatus(500); }
+    res.send(docs);
+  })
 })
 
 //2.3 add base route for questions
 app.get('/question/:id', (req, res) => {
   console.log(req.params);
-  res.send(questions.find((question)=>{return question.id === Number(req.params.id)}));
+  db.collection('questions').findOne({ _id: ObjectID(req.params.id)}, (err,docs)=> {if(err) {throw err} res.send(docs)})
 })
 
 //3.3 add post route
 app.post('/questions', (req, res) => {
   console.log(req.body);
-  questions.push(req.body);
-  res.send(questions);
+  //var question1 = { title: req.body.title };
+
+  //4.4 insert data
+  db.collection("questions").insert(req.body, (err, result) => {
+    if(err){ console.log(err); return res.sendStatus(500); }
+    res.send(req.body);
+    
+  })
 })
 
 //3.4 add put route
 app.put('/question/:id', (req, res) => {
-  var question = questions.find((question) => { return question.id === Number(req.params.id) });
-  question.title = req.body.title;
-  res.sendStatus(200);
+
+  //4.7 insert update()
+  db.collection("questions").update(
+    { _id: ObjectID(req.params.id) },
+    { title: req.body.title },
+    (err, result) => { if(err) throw err; res.sendStatus(200); }
+  )
 })
+
 
 //3.5 add delete route
 app.delete('/question/:id', (req, res) => {
-  questions = questions.filter((question)=> { return question.id !== Number(req.params.id); })
-  res.sendStatus(200);
+  //4.8 insert delete()
+  db.collection("questions").deleteOne(
+    { _id: ObjectID(req.params.id) },
+    (err, result) => { if(err) throw err; res.sendStatus(200);}
+  )
 })
 
 
-//4.4 db initialize and include app.listen()
-MongoClient.connect('mongodb://localhost:27017/myapi', (err, database) => {
-  if(err) { return console.log(err); }
-  db = database; 
+//4.3 db initialize and include app.listen()
+MongoClient.connect('mongodb://localhost:27017/', (err, client) => {
+  if(err) throw err;
+
+  //For mongodb >= 3.0.0: define db-name
+  db = client.db('myapi'); 
 
   //1.4 add listen
   app.listen(3012, function() {
